e9789e1a01e3dd463adeec133953e552ea15d3e0
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-10-22 10:08:48 +0600

    Environment setup

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD with PHP OOP & PDO</title>
+    <link rel="stylesheet" href="inc/bootstrap.min.css" />
+    <script src="inc/bootstrap.bundle.min.js"></script>
+    <script src="inc/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="text-center">
+            <h1>Student List</h1>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                showing student data
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h3>Website: https://coders24x7.com/
+                    <span class="float-end">Link Us: www.facebook.com/........</span>
+                </h3>
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
