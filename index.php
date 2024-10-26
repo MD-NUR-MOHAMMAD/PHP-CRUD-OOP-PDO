b677e64ebd40d47e03d0e57b483444bc47071b9a
MD-NUR-MOHAMMAD <152972252+MD-NUR-MOHAMMAD@users.noreply.github.com>
2024-10-26 18:57:38 +0600

    Table added.

--- index.php
+++ index.php
@@ -12,13 +12,54 @@
 
 <body>
     <div class="container">
-        <div class="text-center">
-            <h1>Student List</h1>
+        <div class="card text-center">
+            <h1>CRUD with PHP OOP & PDO</h1>
+            <h2>Student List</h2>
         </div>
         <div class="card">
             <div class="card-header">
-                showing student data
+                <h2>Student data <a class="float-end" href="addstudent.php">Add Student Data</a></h2>
             </div>
+            <table class="table table-striped">
+                <tr>
+                    <th>Serial</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Action</th>
+                </tr>
+                <tr>
+                    <td>01</td>
+                    <td>Nurmohammad</td>
+                    <td>nur@gmail.com</td>
+                    <td>01677772333</td>
+                    <td>
+                        <a href="editstudent.php?id=1">Edit</a> ||
+                        <a href="deletestudent.php?id=1" onclick="return confirm('Are you sure to Delete?')">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>02</td>
+                    <td>Asif</td>
+                    <td>asif@gmail.com</td>
+                    <td>01677772233</td>
+                    <td>
+                        <a href="editstudent.php?id=2">Edit</a> ||
+                        <a href="deletestudent.php?id=2" onclick="return confirm('Are you sure to Delete?')">Delete</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>03</td>
+                    <td>Rana</td>
+                    <td>rana@gmail.com</td>
+                    <td>01677772331</td>
+                    <td>
+                        <a href="editstudent.php?id=3">Edit</a> ||
+                        <a href="deletestudent.php?id=3" onclick="return confirm('Are you sure to Delete?')">Delete</a>
+                    </td>
+                </tr>
+
+            </table>
         </div>
         <div class="card">
             <div class="card-body">
